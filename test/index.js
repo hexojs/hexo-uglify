74ef9cc26bf73a5f9db2c1f3b5d8db7b8b5d652d
Tommy Chen <tommy351@gmail.com>
2015-01-28 12:10:01 +0800

    Main program done (not tested yet)

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var assign = require('object-assign');
+var filter = require('./lib/filter');
+
+hexo.config.uglify = assign({
+  mangle: true,
+  output: {},
+  compress: {}
+}, hexo.config.uglify);
+
+hexo.extend.filter.register('after_render:js', filter);
