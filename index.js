eec1353485cf1c0c94b7cbb8f85741f48b046c40
curbengh <43627182+curbengh@users.noreply.github.com>
2019-07-28 19:00:43 +0930

    refactor: replace minimatch with micromatch

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ hexo.config.uglify = Object.assign({
   mangle: true,
   output: {},
   compress: {},
-  exclude: ['*.min.js']
+  exclude: '*.min.js'
 }, hexo.config.uglify);
 
 hexo.extend.filter.register('after_render:js', require('./lib/filter'));
