6b39c7fc72f51dec4ec00eb2c6984e093e682959
SukkaW <isukkaw@gmail.com>
2019-11-07 21:43:17 +0800

    test: bring up

--- test/index.js
+++ test/index.js
@@ -1,10 +1,153 @@
 'use strict';
 
-const should = require('chai').should(); // eslint-disable-line
+require('chai').should();
 
-describe('hexo-uglifyjs', () => {
-  // Tests.
-  it('should uglify js.', () => {
-    return true;
+describe('hexo-uglify', () => {
+  const uglifyFilter = require('../lib/filter-uglifyjs');
+  const terserFilter = require('../lib/filter-terser');
+
+  it('es5 - uglify (default options)', () => {
+    const ctx = {
+      config: {
+        uglify: {
+          mangle: true,
+          output: {},
+          compress: {},
+          exclude: '*.min.js'
+        }
+      }
+    };
+
+    const code = `
+    var x = {
+        baz_: 0,
+        foo_: 1,
+        calc: function() {
+            return this.foo_ + this.baz_;
+        }
+    };
+    x.bar_ = 2;
+    x["baz_"] = 3;
+    console.log(x.calc());`;
+
+    const result = uglifyFilter.apply(ctx, [code, { path: 'source/test.js' }]);
+
+    result.should.eql('var x={baz_:0,foo_:1,calc:function(){return this.foo_+this.baz_},bar_:2,baz_:3};console.log(x.calc());');
+  });
+
+  it('es5 - terser (default options)', () => {
+    const ctx = {
+      config: {
+        uglify: {
+          mangle: true,
+          output: {},
+          compress: {},
+          exclude: '*.min.js'
+        }
+      }
+    };
+
+    const code = `
+    var x = {
+        baz_: 0,
+        foo_: 1,
+        calc: function() {
+            return this.foo_ + this.baz_;
+        }
+    };
+    x.bar_ = 2;
+    x["baz_"] = 3;
+    console.log(x.calc());`;
+
+    const result = terserFilter.apply(ctx, [code, { path: 'source/test.js' }]);
+
+    result.should.eql('var x={baz_:(0,3),foo_:1,calc:function(){return this.foo_+this.baz_},bar_:2};console.log(x.calc());');
+  });
+
+  it('es6 - terser (default options)', () => {
+    const ctx = {
+      config: {
+        uglify: {
+          mangle: true,
+          output: {},
+          compress: {},
+          exclude: '*.min.js'
+        }
+      }
+    };
+
+    const code = `
+    const x = {
+        baz_: 0,
+        foo_: 1,
+        calc() {
+            return this.foo_ + this.baz_;
+        }
+    };
+    x.bar_ = (a) => a + 2;
+    x["baz_"] = 3;
+    console.log(x.calc());`;
+
+    const result = terserFilter.apply(ctx, [code, { path: 'source/test.js' }]);
+
+    result.should.eql('const x={baz_:(0,3),foo_:1,calc(){return this.foo_+this.baz_},bar_:o=>o+2};console.log(x.calc());');
+  });
+
+  it('exclude - uglify should ignore *.min.js by default', () => {
+    const ctx = {
+      config: {
+        uglify: {
+          mangle: true,
+          output: {},
+          compress: {},
+          exclude: '*.min.js'
+        }
+      }
+    };
+
+    const code = `
+    var x = {
+        baz_: 0,
+        foo_: 1,
+        calc: function() {
+            return this.foo_ + this.baz_;
+        }
+    };
+    x.bar_ = 2;
+    x["baz_"] = 3;
+    console.log(x.calc());`;
+
+    const result = uglifyFilter.apply(ctx, [code, { path: 'source/test.min.js' }]);
+
+    result.should.eql(code);
+  });
+
+  it('exclude - terser should ignore *.min.js by default', () => {
+    const ctx = {
+      config: {
+        uglify: {
+          mangle: true,
+          output: {},
+          compress: {},
+          exclude: '*.min.js'
+        }
+      }
+    };
+
+    const code = `
+    var x = {
+        baz_: 0,
+        foo_: 1,
+        calc: function() {
+            return this.foo_ + this.baz_;
+        }
+    };
+    x.bar_ = 2;
+    x["baz_"] = 3;
+    console.log(x.calc());`;
+
+    const result = terserFilter.apply(ctx, [code, { path: 'source/test.min.js' }]);
+
+    result.should.eql(code);
   });
 });
